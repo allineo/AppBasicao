e855fc62ecafe057c9523d36fbbc21db338b679f
Alline Oliveira <alline.oliveira@gmail.com>
2019-06-20 13:46:21 +0000

    First commit

--- py/authentication.py
+++ py/authentication.py
@@ -5,42 +5,46 @@ import datetime
 
 
 def verifyFbToken(fbtoken, fbuserid):
-	clientId = getConfig('fbclientId')
-	clientSecret = getConfig('fbclientSecret')
-	apptokenurl = 'https://graph.facebook.com/oauth/access_token?client_id=' + clientId + '&client_secret=' + clientSecret + '&grant_type=client_credentials'
-	appToken = requests.get(apptokenurl).json()['access_token']
-	debugtokenurl = 'https://graph.facebook.com/v3.2/debug_token?input_token=' + fbtoken + '&access_token=' + str(appToken)
-	userId = requests.get(debugtokenurl).json()['data']['user_id']
-	print('Logged User: ' + userId)
-	if userId == fbuserid:
-		#profileurl = 'https://graph.facebook.com/v3.2/' + fbuserid + '?fields=first_name,last_name,profile_pic,email&access_token=' + fbtoken
-		#profile = requests.get(profileurl).json()
-		return userId
-	else:
-		return 'error'
+    clientId = getConfig('fbclientId')
+    clientSecret = getConfig('fbclientSecret')
+    apptokenurl = ('https://graph.facebook.com/oauth/access_token?client_id='
+                   + clientId + '&client_secret=' + clientSecret
+                   + '&grant_type=client_credentials')
+    appToken = requests.get(apptokenurl).json()['access_token']
+    debugtokenurl = ('https://graph.facebook.com/v3.2/debug_token?input_token='
+                     + fbtoken + '&access_token=' + str(appToken))
+    userId = requests.get(debugtokenurl).json()['data']['user_id']
+    print('Logged User: ' + userId)
+    if userId == fbuserid:
+        # profileurl = ('https://graph.facebook.com/v3.2/' + fbuserid +
+        #               '?fields=first_name,last_name,profile_pic,email&access_token='
+        #               + fbtoken)
+        # profile = requests.get(profileurl).json()
+        return userId
+    return 'error'
 
 
 def verifyGlToken(gltoken, gluserid):
-	tokeninfo_url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + gltoken
-	userid = requests.get(tokeninfo_url).json()['sub']
-	if userid == gluserid:
-		return userid
-	else:
-		return 'error'
+    tokeninfo_url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + gltoken
+    userid = requests.get(tokeninfo_url).json()['sub']
+    if userid == gluserid:
+        return userid
+    else:
+        return 'error'
 
 
 def saveUser(request):
-	data = {"login": request.args.get("login"),
-			"userid": request.args.get("userid"),
-			"latitude": request.args.get("latitude"),
-			"longitude": request.args.get("longitude"),
-			"name": request.args.get("username"),
-			"email": request.args.get("useremail"),
-			"timestamp": datetime.datetime.utcnow()}
-	dbcollection = dbConnection("users")
-	user = dbcollection.find_one({"userid": data["userid"]})
-	if user == '' or user == None:
-		dbuserid = dbcollection.insert_one(data).inserted_id
-	else:
-		dbuserid = user["_id"]
-	return str(dbuserid)
+    data = {"login": request.args.get("login"),
+            "userid": request.args.get("userid"),
+            "latitude": request.args.get("latitude"),
+            "longitude": request.args.get("longitude"),
+            "name": request.args.get("username"),
+            "email": request.args.get("useremail"),
+            "timestamp": datetime.datetime.utcnow()}
+    dbcollection = dbConnection("users")
+    user = dbcollection.find_one({"userid": data["userid"]})
+    if user in ('', None):
+        dbuserid = dbcollection.insert_one(data).inserted_id
+    else:
+        dbuserid = user["_id"]
+    return str(dbuserid)
--- py/dbconnect.py
+++ py/dbconnect.py
@@ -2,21 +2,21 @@ import pymongo, json
 
 
 def getConfig(configitem):
-	with open('py/config.json', 'r') as f:
-		config = json.load(f)
-	return config[configitem]
+    with open('py/config.json', 'r') as f:
+        config = json.load(f)
+    return config[configitem]
 
 
 def dbConnection(dbcollectionname):
-	dburi = getConfig('dburi')
-	dbname = "test"
-	client = pymongo.MongoClient(dburi)
-	db = client[dbname]	
-	dbcollection = db[dbcollectionname]
-	return dbcollection
+    dburi = getConfig('dburi')
+    dbname = "test"
+    client = pymongo.MongoClient(dburi)
+    db = client[dbname]
+    dbcollection = db[dbcollectionname]
+    return dbcollection
 
 
 def saveData(dbcollectionname, data):
-	dbcollection = dbConnection(dbcollectionname)
-	id = dbcollection.insert_one(data).inserted_id
-	return str(id)
+    dbcollection = dbConnection(dbcollectionname)
+    id = dbcollection.insert_one(data).inserted_id
+    return str(id)
